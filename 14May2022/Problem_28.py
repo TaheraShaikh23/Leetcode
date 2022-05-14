5c7f636081610784d0e3342bc31d00b47ddcbd4e
tahera <shaikh.tahera.i@gmail.come>
2022-05-14 18:22:42 +0530

    master

---
+++ 14May2022/Problem_28.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/implement-strstr/
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        
+#         for i in range(len(haystack) + 1 - len(needle)):
+#             for j in range(len(needle)):
+#                 if haystack[i + j] != needle[j]:
+#                     break
+#                 if j == len(needle) - 1:
+#                     return i
+#         return -1
+# Time O(n*m)
+# Space(1)
+        
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i: i + len(needle)] == needle:
+                return i
+        return -1
